74e0ac047ac982b015463b921d2267af76b81949
Dorn Moore <dorn@savingcranes.org>
2023-09-25 07:58:07 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,8 @@
+# parameters for ARGOS Data Collection
 argosUser = 'argosUser'
 argosPass = 'argosPass'
 argosProgram = '1234'
+
+# parameters for Postgres Database
 db_string = "postgresql+psycopg2://USER:PASSWORD@HOST:5432/DATABASE" 
+insert_table = 'table_name'
